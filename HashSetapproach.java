b5fef9e379955378dfd9a3936c827730e91b0ede
Ritika Sahay <113190073+SahayRitika@users.noreply.github.com>
2024-03-09 10:02:00 +0530

    Create HashSetapproach.java

---
+++ HashSetapproach.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1= new HashSet<>();
+        Set<Integer> set2= new HashSet<>();
+
+        for(int num: nums1){
+            set1.add(num);
+        }
+
+        for(int num: nums2){
+            if(set1.contains(num)){
+                set2.add(num);
+            }
+        }
+
+        int arr[]= new int[set2.size()];
+        int ind=0;
+        for(Integer x: set2){
+            arr[ind++]=x;
+        }
+        return arr;
+    }
+}
